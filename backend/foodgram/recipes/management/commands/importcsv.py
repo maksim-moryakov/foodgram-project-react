73eb4c98f8198c7cb0a6637a87d5f386f7a8e9b8
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-09-29 15:53:10 +0300

    v4.2

--- backend/foodgram/recipes/management/commands/importcsv.py
+++ backend/foodgram/recipes/management/commands/importcsv.py
@@ -1,46 +1,18 @@
-import csv
-from os import path
+import json
 
 from django.core.management.base import BaseCommand
-from foodgram.settings import STATIC_ROOT
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    """Обработчик менеджмент-команды по импорту csv-данных в БД."""
+    ''' Загрузка данных из JSON-файла '''
+
     def handle(self, *args, **options):
-        data_path = path.join(STATIC_ROOT, 'data')
-        self.import_ingredients(path.join(data_path, 'ingredients.csv'))
+        self.stdout.write(self.style.WARNING('Загружаем ингридиенты!'))
+        with open('data/ingredients.json', encoding='utf-8',
+                  ) as data_file_ingredients:
+            ingredient_data = json.loads(data_file_ingredients.read())
+            for ingredients in ingredient_data:
+                Ingredient.objects.get_or_create(**ingredients)
 
-    def import_csv_file(self, conn, table_name, csv_file):
-        cur = conn.cursor()
-        header = ''
-        with open(csv_file, 'r', encoding='utf-8') as read_file:
-            reader = csv.reader(read_file)
-            for row in reader:
-                if header == '':
-                    header = ','.join(row)
-                    col_count = len(row)
-                    questions = ['?'] * col_count
-                    questions_str = ','.join(questions)
-                else:
-                    insert_sql_str = (
-                        f'INSERT INTO {table_name} ({header}) '
-                        f'VALUES ({questions_str});'
-                    )
-                    self.stdout.write(insert_sql_str)
-                    cur.execute(insert_sql_str, row)
-        conn.commit()
-
-    def import_ingredients(self, csv_file):
-        Ingredient.objects.all().delete()
-        with open(csv_file, 'r', encoding='utf-8') as read_file:
-            reader = csv.DictReader(read_file)
-            id = 1
-            for row in reader:
-                Ingredient.objects.get_or_create(
-                    id=id,
-                    name=row['name'],
-                    measurement_unit=row['measurement_unit'],
-                )
-                id += 1
+        self.stdout.write(self.style.SUCCESS('Ингридиенты загружены!'))
